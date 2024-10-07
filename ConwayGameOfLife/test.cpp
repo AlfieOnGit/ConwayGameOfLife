57e9079a5c6b6310136f5fb14ea5802431c5722f
Alfie <alfiejay@mail.com>
2024-10-07 16:20:03 +0100

    Add decent test case (Make bugs more obvious, pls fix)

--- ConwayGameOfLife/test.cpp
+++ ConwayGameOfLife/test.cpp
@@ -5,7 +5,11 @@
 int main()
 {
     auto const *grid = new Grid(5, 5);
-    grid->set(1, 1, true);
+    grid->set(4, 4, true);
+    grid->set(3, 3, true);
+    grid->set(2, 3, true);
+    grid->set(3, 2, true);
+    grid->set(4, 2, true);
     grid->print();
     grid->tick();
     grid->print();
