80ce5d456cbccad03f273a1ad0185722dd71546c
Alfie <alfiejay@mail.com>
2024-10-08 13:29:43 +0100

    Add FlipStack

---
+++ ConwayGameOfLife/FlipStack/FlipStack.cpp
@@ -0,0 +1,61 @@
+﻿#include "FlipStack.h"
+
+template <typename T>
+FlipStack<T>::FlipStack(int max_height)
+{
+    this->max_height = max_height;
+    left_stack = new T[max_height];
+    right_stack = new T[max_height];
+}
+
+template <typename T>
+FlipStack<T>::~FlipStack()
+{
+    delete[] left_stack;
+    delete[] right_stack;
+}
+
+template <typename T>
+void FlipStack<T>::flip()
+{
+    currently_left = !currently_left;
+}
+
+template <typename T>
+bool FlipStack<T>::append(T item)
+{
+    if (get_current_height() >= max_height) return false;
+    get_current_stack()[get_current_height()++] = item;
+    return true;
+}
+
+template <typename T>
+T FlipStack<T>::pop()
+{
+    if (get_current_height() == 0) return nullptr;
+    T hold = get_current_stack()[get_current_height()];
+    get_current_stack()[get_current_height()--] = nullptr;
+    return hold;
+}
+
+template <typename T>
+void FlipStack<T>::clear_current_stack()
+{
+    for (int i = 0; i < get_current_height(); i++)
+    {
+        get_current_stack()[i] = nullptr;
+    }
+    get_current_height() = 0;
+}
+
+template <typename T>
+T *&FlipStack<T>::get_current_stack()
+{
+    return currently_left ? left_stack : right_stack;
+}
+
+template <typename T>
+int& FlipStack<T>::get_current_height()
+{
+    return currently_left ? left_height : right_height;
+}
---
+++ ConwayGameOfLife/FlipStack/FlipStack.h
@@ -0,0 +1,25 @@
+﻿#pragma once
+
+// FlipStack holds two stacks and allows the user to flip() between the two. The idea is to save the user from having
+// to keep checking which of the two stacks they should be handling at a given time.
+template <typename T> class FlipStack
+{
+public:
+    // Parameter "max_height" is the max height of both stacks INDIVIDUALLY, not combined.
+    FlipStack(int max_height);
+    ~FlipStack();
+    // Flips the focus between the left and right stacks.
+    void flip();
+    // Takes argument and inserts it onto the top of the current stack. Returns TRUE if successful, FALSE if the stack
+    // is full.
+    bool append(T item);
+    // Removes and returns the top item from the current stack. Returns item or NULLPTR if the stack is empty.
+    T pop();
+    void clear_current_stack();
+private:
+    bool currently_left = true;
+    int max_height, left_height { 0 }, right_height { 0 };
+    T *left_stack, *right_stack;
+    T *&get_current_stack();
+    int &get_current_height();
+};
