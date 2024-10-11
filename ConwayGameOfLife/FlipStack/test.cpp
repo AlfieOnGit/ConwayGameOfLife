e85d2096701e66b744c6472999044e3352312e5b
Alfie <alfiejay@mail.com>
2024-10-11 16:39:07 +0100

    Comment out unused code (all of it)

--- ConwayGameOfLife/FlipStack/test.cpp
+++ ConwayGameOfLife/FlipStack/test.cpp
@@ -2,17 +2,28 @@
 
 #include "FlipStack.h"
 
-int main()
-{
-    FlipStack<int> *flip_stack = new FlipStack<int>(10);
-    flip_stack->append(1);
-    flip_stack->append(2);
-    flip_stack->append(3);
-    std::cout << flip_stack->pop() << ", ";
-    flip_stack->flip();
-    std::cout << flip_stack->pop() << ", ";
-    flip_stack->flip();
-    std::cout << flip_stack->pop() << '\n';
+//FlipStack<int> flip_stack(10);
+
+// void test_operators()
+// {
+//     flip_stack << 1 << 2 << 3 << 4 << 5;
+//     int a, b, c;
+//     flip_stack >> a >> b >> c;
+//     std::cout << "A = " << a << ", B = " << b << ", C = " << c << "!\n";
+//     std::cout << "New height = " << flip_stack.get_height() << '\n';
+//     flip_stack.clear();
+//     flip_stack << 10 << 20 << 30 << 40;
+//     flip_stack >> a >> b >> c;
+//     std::cout << "A = " << a << ", B = " << b << ", C = " << c << "!\n";
+//     std::cout << "New height = " << flip_stack.get_height() << '\n';
+// }
+
+//int main()
+//{
+    //test_operators();
+    //std::cout << flip_stack.bad_pop();
+    //auto *arr = new int[2];
+    //std::cout << "Arr[0] = " << arr[0] << std::endl;
     
-    return 0;
-}
+//    return 0;
+//}
