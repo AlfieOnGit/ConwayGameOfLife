f1ec728c1b9e64ed1d40ce0374a351551acfbf7b
Alfie <67489447+AlfieOnGit@users.noreply.github.com>
2024-10-18 06:20:32 +0100

    Update ConwayGrid.h

--- ConwayGameOfLife/Grid/ConwayGrid.h
+++ ConwayGameOfLife/Grid/ConwayGrid.h
@@ -10,11 +10,11 @@ public:
     void print();
     // Places COUNT number of cells randomly about the grid.
     void populate(int count) const;
-    void flip(int x, int y);
+    inline void flip(int x, int y);
     void tick();
     void tick_and_print();
 protected:
     FlipStack<Coordinate> *flip_stack; // Holds coordinates that need flipping (killing or reviving)
     bool needs_flip(Coordinate coord) const;
-    void new_row();
+    void new_column();
 };
